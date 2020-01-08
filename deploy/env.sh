ed78898c7bde6589c22b4be1823eef9ce934da21
Ding Li <1995dingli@gmail.com>
2020-01-08 09:32:41 +0800

    Update env.sh

--- deploy/env.sh
+++ deploy/env.sh
@@ -1,3 +1,3 @@
-apt-get update && apt-get install -y curl && curl -fsSL https://get.docker.com | sh
-curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo apt-get update && sudo apt-get install -y curl && sudo curl -fsSL https://get.docker.com | sh
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
