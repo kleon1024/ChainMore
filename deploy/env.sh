c157cec8c7636e06806cb0ebf433479f9c399d73
Ding Li <1995dingli@gmail.com>
2020-01-08 23:08:15 +0800

    Add Database URL Config

--- deploy/env.sh
+++ deploy/env.sh
@@ -1,3 +1,4 @@
 sudo apt-get update && sudo apt-get install -y curl && sudo curl -fsSL https://get.docker.com | sh
 sudo curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+mkdir ~/chainmore
