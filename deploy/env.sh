11b8a84f728cb5d5ad5c6a7d8f91b85a188ed414
Ding Li <1995dingli@gmail.com>
2020-01-09 00:11:56 +0800

    Clean Env

--- deploy/env.sh
+++ deploy/env.sh
@@ -1,4 +1,6 @@
 sudo apt-get update && sudo apt-get install -y curl && sudo curl -fsSL https://get.docker.com | sh
 sudo curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-mkdir ~/chainmore
+sudo apt-get install -y git python3 python3-pip python3-venv
+
+git clone https://github.com/kleon1024/ChainMore-API-Flask $HOME/chainmore
