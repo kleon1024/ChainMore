296953395dee1acb3e895919d24fd3b52375a163
Ding Li <1995dingli@gmail.com>
2020-08-19 00:38:59 +0800

    add db migrate

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 # docker-compose -f nginx-compose.yml up -d
+docker exec chainmore_backend_1 flask db migrate
+docker exec chainmore_backend_1 flask db upgrade
 docker-compose -f deploy-compose.yml pull
 docker-compose -f deploy-compose.yml up -d
