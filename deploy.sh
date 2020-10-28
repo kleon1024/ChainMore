1bab56ddacf74314a8bc341ddf6a7ff4e9f601f0
Ding Li <1995dingli@gmail.com>
2020-10-28 13:59:57 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 # docker-compose -f nginx-compose.yml up -d
-docker exec chainmore_backend_1 flask db migrate
-docker exec chainmore_backend_1 flask db upgrade
 docker-compose -f deploy-compose.yml pull
 docker-compose -f deploy-compose.yml up -d
+docker exec chainmore_backend_1 flask db migrate
+docker exec chainmore_backend_1 flask db upgrade
+docker exec chainmore_backend_1 flask init
